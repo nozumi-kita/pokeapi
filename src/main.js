55dc9725fd77819ffa16b7bd85464164e3d9621d
nozumi <minirogo2052.igo@gmail.com>
2026-08-05 17:34:56 +0900

    検索したポケモンに1進化前があるなら、その情報も表示。

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const form = document.getElementById("form-js");
+const url = "https://pokeapi.co/api/v2/pokemon/";
 
 const pokeFunction = async () => {
   const inputPoke = document.getElementById("poke-name-js");
@@ -7,27 +8,47 @@ const pokeFunction = async () => {
   result.innerHTML = "";
 
   try {
-    const url = `https://pokeapi.co/api/v2/pokemon/${inputName}`;
-    const response = await fetch(url);
-    const data = await response.json();
+    const pokeUrl = `${url}${inputName}`;
+    const responsePoke = await fetch(pokeUrl);
+    const dataPoke = await responsePoke.json();
 
-    const pokeName = document.createElement("p");
-    pokeName.textContent = `Name: ${data.name}`;
+    const speciesUrl = dataPoke.species.url;
+    const responseSpecies = await fetch(speciesUrl);
+    const dataSpecies = await responseSpecies.json();
 
-    const pokeImg = document.createElement("img");
-    pokeImg.src = data.sprites.front_default;
-    pokeImg.alt = data.name;
+    renderPoke(dataPoke, result);
 
-    const pokeId = document.createElement("p");
-    pokeId.textContent = `ID: ${data.id}`;
+    if (dataSpecies.evolves_from_species) {
+      const evolutionFromName = dataSpecies.evolves_from_species.name;
+      const responseEvolutionFrom = await fetch(`${url}${evolutionFromName}`);
+      const dataEvolutionFrom = await responseEvolutionFrom.json();
 
-    const pokeType = document.createElement("p");
-    pokeType.textContent = `Type: ${data.types.map((typeInfo) => typeInfo.type.name).join(", ")}`;
-
-    result.append(pokeName, pokeImg, pokeId, pokeType);
+      renderPoke(dataEvolutionFrom, result, "進化前");
+    }
   } catch (error) {
     alert("通信に失敗しました");
   }
+};
+
+const renderPoke = (data, result, evolve = "") => {
+  if (evolve) {
+    const evolveFrom = document.createElement("p");
+    evolveFrom.textContent = evolve;
+  }
+  const pokeName = document.createElement("p");
+  pokeName.textContent = `Name: ${data.name}`;
+
+  const pokeImg = document.createElement("img");
+  pokeImg.src = data.sprites.front_default;
+  pokeImg.alt = data.name;
+
+  const pokeId = document.createElement("p");
+  pokeId.textContent = `ID: ${data.id}`;
+
+  const pokeType = document.createElement("p");
+  pokeType.textContent = `Type: ${data.types.map((typeInfo) => typeInfo.type.name).join(", ")}`;
+
+  result.append(evolve, pokeName, pokeImg, pokeId, pokeType);
 };
 
 form.addEventListener("submit", (event) => {
