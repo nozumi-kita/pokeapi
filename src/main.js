a92b73392fc8dd2e64f909456cb2ca7f7bf74f23
nozumi <minirogo2052.igo@gmail.com>
2026-08-05 15:00:49 +0900

    見本の機能実装

---
+++ src/main.js
@@ -0,0 +1,42 @@
+const form = document.getElementById("form-js");
+
+const pokeFunction = async () => {
+  const inputPoke = document.getElementById("poke-name-js");
+  const inputName = inputPoke.value;
+  const result = document.getElementById("result-js");
+  result.innerHTML = "";
+
+  try {
+    const url = `https://pokeapi.co/api/v2/pokemon/${inputName}`;
+
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error("通信に失敗しました");
+    }
+
+    const data = await response.json();
+
+    const pokeName = document.createElement("p");
+    pokeName.textContent = `Name: ${data.name}`;
+
+    const pokeImg = document.createElement("img");
+    pokeImg.src = data.sprites.front_default;
+    pokeImg.alt = data.name;
+
+    const pokeId = document.createElement("p");
+    pokeId.textContent = `ID: ${data.id}`;
+
+    const pokeType = document.createElement("p");
+    pokeType.textContent = `Type: ${data.types.map((typeInfo) => typeInfo.type.name).join(", ")}`;
+
+    const id = result.append(pokeName, pokeImg, pokeId, pokeType);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  pokeFunction();
+});
